514a3a4bec8adaf2f576d612bfe23470f16f6e53
gabrieelbrazao <gabrieelbrazao@gmail.com>
2021-06-15 00:13:02 -0300

    add consultation tests

--- test/consulta.spec.ts
+++ test/consulta.spec.ts
@@ -1,13 +1,37 @@
+import { ConsultationFactory } from 'Database/factories/Consulta'
 import test from 'japa'
 
 test.group('Consulta', () => {
-  // manda todos os dados, exceto o id e retorna nada
-  test('Deverá adicionar uma consulta no banco de dados', (assert) => {
-    assert
+  test('Deverá adicionar uma consulta no banco de dados', async (assert) => {
+    const consultation = await ConsultationFactory.with('ficha', 1, (record) =>
+      record.with('cliente', 1, (client) => client.with('usuario'))
+    ).create()
+
+    assert.isTrue(consultation.$isPersisted)
   })
 
-  // manda o id da ficha, retorna todas as consultas
-  test('Deverá retornar todas as consultas da ficha', (assert) => {
-    assert
+  test('Deverá adicionar uma consulta sem campos opcionais no banco de dados', async (assert) => {
+    const consultation = await ConsultationFactory.apply('optionals')
+      .with('ficha', 1, (record) => record.with('cliente', 1, (client) => client.with('usuario')))
+      .create()
+
+    assert.isTrue(consultation.$isPersisted)
+  })
+
+  test('Deverá retornar todas as consultas da ficha', async (assert) => {
+    const consultations = await ConsultationFactory.with('ficha', 1, (record) =>
+      record.with('cliente', 1, (client) => client.with('usuario'))
+    ).createMany(5)
+
+    let allConsultationsArePersisted = true
+
+    consultations.map((consultation) => {
+      if (!consultation.$isPersisted) allConsultationsArePersisted = false
+    })
+
+    assert.plan(2)
+
+    assert.isTrue(allConsultationsArePersisted)
+    assert.equal(consultations.length, 5)
   })
 })
